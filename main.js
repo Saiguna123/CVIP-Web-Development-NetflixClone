bffa76a62c2628c0b6b9c915c9b7382475a0cc04
Saiguna123 <111770632+Saiguna123@users.noreply.github.com>
2023-11-02 20:41:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function myFunction() {
+    var x = document.getElementById("btncontent");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    changeheight();
+  }
+function myFunction1() {
+    var x = document.getElementById("btncontent1");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function myFunction2() {
+    var x = document.getElementById("btncontent2");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function myFunction3() {
+    var x = document.getElementById("btncontent3");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function myFunction4() {
+    var x = document.getElementById("btncontent4");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+function myFunction5() {
+    var x = document.getElementById("btncontent5");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function changeheight() {
+    var y = document.getElementsByClassName("question");
+    y.style.height = "900px";
+}
